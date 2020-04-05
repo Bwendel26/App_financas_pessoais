1d77e9d843d9564e62127517294669bac33e5ef8
Bruno Fernandes <bwendel26999@gmail.com>
2020-04-04 22:12:50 -0300

    Modificacoes nos arquivos app.js (na logica de insert, criando objeto que faz a persistencia dos dados em localstorage, e controle de entrada de insert dos rergistros) e em index.html (insert de modais bootstrap para feedback de insert).

--- app.js
+++ app.js
@@ -8,7 +8,44 @@ class Despesa {
       this.descricao = descricao;
       this.valor = valor;
    }
+
+   validarDados() {
+      for(let i in this) {
+         //console.log(i, this[i]); //acessando atributos de objetos
+         if(this[i] == undefined || this[i] == '' || this[i] == null) {
+            return false;
+         }
+         return true;
+      }
+   }
 }
+
+class Bd {
+   constructor() {
+      let id = localStorage.getItem('id');
+
+      if(id == null) {
+         localStorage.setItem('id', parseInt(0));
+      } //iniciando valor pra id, como a informação nao existe
+      // até a construção do objeto em localstorage
+   }
+
+   getProximoId() {
+      let proximoId = localStorage.getItem('id'); //antes do constructor atuar seria null
+      return parseInt(proximoId) + 1;
+   }
+
+   gravar(d) {
+      // localStorage.setItem('despesa', JSON.stringify(d));
+      let id = this.getProximoId();
+
+      localStorage.setItem(id, JSON.stringify(d));
+
+      localStorage.setItem('id', id);
+   }
+}
+
+let bd = new Bd(); //instanciando
 
 function cadastrarDespesa(){
    
@@ -29,13 +66,18 @@ function cadastrarDespesa(){
       descricao.value,
       valor.value
    )
-
-   gravar(despesa);
+   //controle e feedback de gravação da despesa
+   if(despesa.validarDados()) {
+      bd.gravar(despesa);
+      $('#sucessoGravacao').modal('show');
+   } else { 
+      $('#erroGravacao').modal('show');
+   }
 }
 
-function gravar(d) {
-   //acessando local storage, armazenamento persistente
-   //por parte do front end.
-   localStorage.setItem('despesa', JSON.stringify(d)) //identificador do item e o proprio item
-   //nesse caso o item esta sempre na mesma chave, sobrepondo se houver mudanças.
-}
+// function gravar(d) {
+//    //acessando local storage, armazenamento persistente
+//    //por parte do front end.
+//    localStorage.setItem('despesa', JSON.stringify(d)) //identificador do item e o proprio item
+//    //nesse caso o item esta sempre na mesma chave, sobrepondo se houver mudanças.
+//
